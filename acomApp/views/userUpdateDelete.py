7b3f8ad854690871302b59f9d4ca2290ed1295ac
maocastro13 <maocastro13@gmail.com>
2022-09-19 15:26:26 -0500

    actualizacion
    CRUD COMPLETO

---
+++ acomApp/views/userUpdateDelete.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework.permissions import IsAuthenticated
+
+from acomApp.models.user import Userpage
+from acomApp.serializers.userSerializer import UserSerializer
+
+class UserUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Userpage.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    
+    
+    def put(self, request, *args, **kwargs):
+        
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
+        
+        if valid_data['user_id'] != kwargs['pk']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+        
+        return super().put(request, *args, **kwargs)
+
+
+    def delete(self, request, *args, **kwargs):
+        
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
+        
+        if valid_data['user_id'] != kwargs['pk']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+        
+        return super().delete(request, *args, **kwargs)
