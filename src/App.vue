180eca551431d0914ea7e22dac153b5317447c8d
Reynaldo Gutierrez <reynaldogutierrezbenitez@gmail.com>
2022-09-20 13:54:51 -0500

    Archivos del frontend

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app" class="app">
+
+    <div class="header">
+      <h1>Banco Mision TIC</h1>
+    </div>
+
+  </div>
 </template>
 
+<script>
+
+export default{
+  name: 'App',
+
+  data: function(){    
+  },
+
+  methods:{
+  },
+
+  created: function(){
+  },
+   
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+  body{
+    margin: 0 0 0 0;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .header{
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+    background-color: #283747;
+    color: #E5E7E9;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
 </style>
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+
+const routes = [{
+path: '/',
+name: 'root',
+component: App
+}]
+
+const router = createRouter({
+history: createWebHistory(),
+routes
+})
+
+export default router
