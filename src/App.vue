522463f498d11ff8608f8048b28ca3d97446fa37
Reynaldo Gutierrez <reynaldogutierrezbenitez@gmail.com>
2022-09-21 14:37:39 -0500

    Actualizacion al LogIn y Pagina principal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
-
 <template>
   <div id="app" class="app">
-
-    <div class="header">
-      <h1>Banco Mision TIC</h1>
-    </div>
-
+  <div class="header">
+  <h1>Banco UN</h1>
+  <nav>
+  <button v-if="is_auth" > Inicio </button>
+  <button v-if="is_auth" > Cuenta </button>
+  <button v-if="is_auth" > Cerrar Sesión </button>
+  <button v-if="!is_auth" v-on:click="loadLogIn" > Iniciar Sesión </button>
+  <button v-if="!is_auth" v-on:click="loadSignUp" > Registrarse </button>
+  </nav>
   </div>
-</template>
+  
+  <div class="main-component">
+  <router-view
+  v-on:completedLogIn="completedLogIn"
+  v-on:completedSignUp="completedSignUp"
+  >
+  </router-view>
+  </div>
+  
+  <div class="footer">
+  <h2>Misión TIC 2022</h2>
+  </div>
+  </div>
+  </template>
 
 <script>
 
 export default{
   name: 'App',
 
-  data: function(){    
+  data: function(){ 
+    return{
+      is_auth: false
+    }   
+  },
+
+  components: {
   },
 
   methods:{
+   verifyAuth: function(){
+    if(this.is_auth == false)
+      this.$router.push({name:"logIn"})
+    },
+    loadLogIn: function(){
+      this.$router.push({name:"logIn"})
+    },
+    loadSignUp: function(){
+      this.$router.push({name:"singUp"})
+    },
+
+    completedLogIn: function(data){},
+    completedSignUp: function(data){},
   },
 
   created: function(){
+    this.verifyAuth()
   },
-   
 }
 </script>
 
@@ -49,5 +84,56 @@ export default{
   .header h1{
     width: 20%;
     text-align: center;
+  }
+  
+  .header nav{
+    height: 100%;
+    width: 20%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    font-size: 20px;
+  }
+  
+  .header nav button{
+    color: #E5E7E9;
+    background: #283747;
+    border: 1px solid #E5E7E9;
+
+    border-radius: 5px;
+    padding: 20px 20px;
+  }
+
+  .header nav button:hover{
+    color:#283747;
+    background: #E5E7E9;
+    border: 1px solid #E5E7E9;
+  }
+
+  .main-component{
+    height: 75vh;
+    margin: 0%;
+    padding: 0%;
+    background: #FDFEFE;
+  }
+
+  .footer{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+
+    background: #283747;
+    color: #E5E7E9;
+  }
+
+  .footer h2{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
+import LogIn from './components/LogIn.vue'
+import SingUp from './components/SingUp.vue'
+
 const routes = [{
 path: '/',
 name: 'root',
 component: App
+},
+{
+    path: '/user/logIn',
+    name: 'logIn',
+    component: LogIn,
+},
+{
+    path: '/user/singUp',
+    name: 'singUp',
+    component: SingUp,
 }]
 
 const router = createRouter({
 history: createWebHistory(),
-routes
-})
+routes,
+});
 
 export default router
